ac1761a6d0a85ff69ed97b3ba34a96cc720d4f8c
TharllesKawa <tharlleskawa22@gmail.com>
2022-09-23 14:40:32 -0300

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#meio', {duration: 1000});
+
+sr.reveal('#sobre', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 1500
+});
+
+sr.reveal('#sociais', {
+    rotate: {x: 0, y: 80, z: 0},
+    duration: 1500
+});
+
+sr.reveal('#serviços', {
+    rotate: {x: 20, y: 100, z: 0},
+    duration: 1500
+});
+
+sr.reveal('#footer', {
+    rotate: {x: 20, y: 100, z: 0},
+    duration: 1500
+});
+
+sr.reveal('#projetos', {
+    rotate: {x: 100, y: 0, z: 100},
+    duration: 1500
+});
